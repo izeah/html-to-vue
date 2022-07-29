f42ecbd3490a355d78bb531f3db4d6e168a6633c
izeah <faizmohhafidza@gmail.com>
2022-07-29 14:32:05 +0700

    added computed property

--- src/elements/PricingItem.vue
+++ src/elements/PricingItem.vue
@@ -10,23 +10,45 @@
         components: {
             Button,
         },
+        computed: {
+            sanitizeItemID() {
+                return typeof this.item.id !== "undefined"
+                    ? this.item.id
+                    : Math.random() * 999999999;
+            },
+            sanitizeItemPrice() {
+                return typeof this.item.price !== "undefined"
+                    ? this.item.price
+                    : 0;
+            },
+            sanitizeItemDesc() {
+                return typeof this.item.desc !== "undefined"
+                    ? this.item.desc
+                    : "[NO DESCRIPTION]";
+            },
+            sanitizeItemPerks() {
+                return typeof this.item.perks !== "undefined"
+                    ? this.item.perks
+                    : [];
+            },
+        },
     };
 </script>
 
 <template>
     <div class="tiles-item">
-        <div class="tiles-item-inner has-shadow" :key="item?.id || 0">
+        <div class="tiles-item-inner has-shadow" :key="sanitizeItemID">
             <div class="pricing-item-content">
                 <div class="pricing-item-header pb-24 mb-24">
                     <div class="pricing-item-price mb-4">
                         <span class="pricing-item-price-currency h2">$</span
                         ><span
                             class="pricing-item-price-amount h1 pricing-switchable"
-                            >{{ item?.price || 0 }}</span
+                            >{{ sanitizeItemPrice }}</span
                         >
                     </div>
                     <div class="text-xs text-color-low">
-                        {{ item?.desc || "Lorem Ipsum Dolor Sit Amet" }}
+                        {{ sanitizeItemDesc }}
                     </div>
                 </div>
                 <div class="pricing-item-features mb-40">
@@ -39,7 +61,7 @@
                         class="pricing-item-features-list list-reset text-xs mb-32"
                     >
                         <li
-                            v-for="perk in item?.perks || []"
+                            v-for="perk in sanitizeItemPerks"
                             :class="perk.isChecked ? 'is-checked' : ''"
                             :key="perk.id"
                         >
